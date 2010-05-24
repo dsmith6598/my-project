bf6eada7ced0f7363205ca96b2b39260f0cc21d4
doug <doug@debian.(none)>
2010-05-23 21:16:51 -0400

    first commit/add all lein stuff

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject leiningen "0.5.0-SNAPSHOT"
+  :description "A build tool designed not to set your hair on fire."
+  :url "http://github.com/technomancy/leiningen"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+                 [ant/ant-launcher "1.6.2"]
+                 [org.apache.maven/maven-ant-tasks "2.0.10"]
+                 [compojure "0.4.0-RC3"]]
+                   
+                  
+:dev-dependencies [[swank-clojure "1.2.1"]])
