8fb8a1539f9b97486d015ccdd2fea9931a8e6970
doug <doug@debian.(none)>
2010-05-25 23:26:03 -0400

    added fleetdb

--- project.clj
+++ project.clj
@@ -5,7 +5,12 @@
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [ant/ant-launcher "1.6.2"]
                  [org.apache.maven/maven-ant-tasks "2.0.10"]
-                 [compojure "0.4.0-RC3"]]
+                 [compojure "0.4.0-RC3"]
+		 [fleetdb "0.2.0-RC1"]
+		 [fleetdb-client "0.2.0-RC1"]
+		 [ring/ring "0.2.0"]
+	       	[ring/ring-core "0.2.0"]]
                    
                   
-:dev-dependencies [[swank-clojure "1.2.1"]])
+   :dev-dependencies [[swank-clojure "1.2.1"]])
+		    
--- src/core.clj
+++ src/core.clj
@@ -1,3 +1,43 @@
 ;;; new file ;;;
 
-(+ 1 2 3)
+(apply hash-map (interleave [:a :b :c] [1 2 3]))
+
+(into {} (map vector [:a :b :c] [1 2 3]))
+
+
+;;; fleetdb ;;;
+
+;; start db with:
+
+;;   $ java -cp fleetdb-standalone.jar fleetdb.server -f demo.fdb
+
+;; To connect the client to the database:
+
+(client ["ping"])
+
+(use 'fleetdb.client)
+(def client (connect {:host "127.0.0.1", :port 3400}))
+
+
+(client ["insert" "accounts" {"id" 1, "owner" "Eve", "credits" 100}])
+
+
+(client ["insert" "accounts"
+          [{"id" 2, "owner" "Bob", "credits" 150}
+           {"id" 3, "owner" "Dan", "credits" 50}
+           {"id" 4, "owner" "Amy", "credits" 1000, "vip" true}]])
+
+
+(client ["select" "accounts" {"where" ["=" "id" 2]}])
+
+(client ["select" "accounts"])
+
+
+(client ["insert" "accounts"
+          [{"id" 52, "owner" "Fred", "credits" 2150}
+           {"id" 53, "owner" "Ethel", "credits" 550}
+           {"id" 54, "owner" "Lucie", "credits" 14000, "vip" false}]])
+
+
+(client ["update" "accounts" {"credits" 55} {"where" ["=" "owner" "Bob"]}])
+
